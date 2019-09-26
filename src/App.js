490db0ff3ea8b6ad31261ab60735ef3df76f0034
Nico Ouwerkerk <NIC@noldusbv.local>
2019-09-26 16:31:43 +0200

    cleaned up some redundant code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
 import { Route } from "react-router";
-import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { setMessage, checkLive } from "./redux/actions";
-import axios from "axios";
 import { Container, Row, Col } from "reactstrap";
 import styled, { ThemeProvider } from "styled-components";
 import LiveView from "./components/LiveView";
 import Header from "./components/Header";
 import ShareMedia from "./components/ShareMedia";
 import theme from "./theme.js";
-import socketIOClient from "socket.io-client";
-
-
-console.log(window.APIaddress);
 
 const MainContainer = styled.div`
   display: flex;
@@ -49,54 +42,7 @@ const MainContent = styled.div`
 `;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      eyeTrackerPresent: false
-    };
-  }
-  displayName = App.name;
-
-  componentDidMount() {
-    if (this.props.location.pathname !== "/caller") {
-      this.props.checkLive(true);
-    }
-    // Start streaming Gaze data
-    axios
-      .post(`${window.APIaddress}/api/eyetracking/start`)
-      .then(function(response) {
-        this.setState({ eyeTrackerPresent: response.data });
-      })
-      .catch(error => {
-        this.props.setMessage("No stream available");
-      });
-
-    // Start stimulus logging
-    axios
-      .post(`${window.APIaddress}/api/stimulus/start`)
-      .then(response => this.props.setMessage(""))
-      .catch(error => {
-        this.props.setMessage("No stimulus logger available");
-      });
-
-
-      const socket = socketIOClient("http://localhost:4001");
-
-     socket.on("outgoing data", data =>console.log("this is " + data));
-
-     
-  }
-
-  // Stop streaming gaze data
-  componentWillUnmount() {
-    axios
-      .post(`${window.APIaddress}/api/eyetracking/stop`)
-      .then(response => console.log(response))
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
+ 
   render() {
     return (
       <ThemeProvider theme={theme}>
@@ -121,22 +67,4 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setMessage: message => dispatch(setMessage(message)),
-    checkLive: live => dispatch(checkLive(live))
-  };
-};
-
-const mapStateToProps = state => {
-  return {
-    message: state.message
-  };
-};
-
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(App)
-);
+export default withRouter(App);
